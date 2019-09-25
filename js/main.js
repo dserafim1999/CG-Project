9bc823ac46070669616fed17fa83ed490bd0c286
jpaquete14 <jpaquete14@gmail.com>
2019-09-25 14:53:05 +0100

    added top, front and side camera

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-function init() {
+function onKeyDown(e) {
 	'use strict';
 	
-	renderer = new THREE.WebGLRenderer( {antialias: true } );
-	
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	
-	document.body.appendChild(renderer.domElement);
-	
-	createScene();
-	createCamera();
-	
-	render();
-	
-	window.addEventListener("resize", onResize);
+	switch (e.keyCode) {
+		case 49://1
+			createTopCamera();
+			break;
+
+		case 50://2
+			createFrontCamera();
+			break;
+
+		case 51://3
+			createSideCamera();
+			break;
+
+		case 52://4
+			scene.traverse(function (node) {
+				if (node instanceof THREE.Mesh) {
+					node.material.wireframe = !node.material.wireframe;
+				}
+			});
+			break;
+	}
 }
 
 function createScene() {
@@ -29,4 +38,22 @@ function createScene() {
 function render() {
 	'use strict';
 	renderer.render(scene, camera);
+}
+
+function init() {
+	'use strict';
+	
+	renderer = new THREE.WebGLRenderer( {antialias: true } );
+	
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	
+	document.body.appendChild(renderer.domElement);
+	
+	createScene();
+	createTopCamera();
+	
+	render();
+	
+	window.addEventListener("resize", onResize);
+	window.addEventListener("keydown", onKeyDown);
 }
