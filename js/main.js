edc6b43b32256840c0ffccaa0eee75bd421cb244
dserafim1999 <48388827+dserafim1999@users.noreply.github.com>
2019-09-28 19:00:41 +0100

    added movement and rotation
    not finished, dependent on teachers input on Monday

--- js/main.js
+++ js/main.js
@@ -2,36 +2,53 @@ function onKeyDown(e) {
 	'use strict';
 	
 	switch (e.keyCode) {
-		
-		case 49: //1
-			scene.activeCamera = TopCamera;
+		case 49://1
+			createTopCamera();
 			break;
-		
-		case 50: //2
-			scene.activeCamera = FrontCamera;
+		case 50://2
+			createFrontCamera();
 			break;
-		
-		case 51: //3
-			scene.activeCamera = SideCamera;
+		case 51://3
+			createSideCamera();
 			break;
-		
-		case 52: //4
+		case 52://4
 			scene.traverse(function (node) {
 				if (node instanceof THREE.Mesh) {
 					node.material.wireframe = !node.material.wireframe;
 				}
 			});
 			break;
+		case 38: //up
+			robot.movement.up = true;
+			break;
+		case 40: //down
+			robot.movement.down = true;
+			break;
+		case 37: //left
+			robot.movement.left = true;
+			break;
+		case 39: //right
+			robot.movement.right = true;
+			break;
+		/*nao esquecer pecas individuais
+		case 65: //A
+		case 97: //a
+			robot.movement.anticlockwise = true;
+			break;
+		case 83: //S
+		case 115: //s
+			robot.movement.clockwise = true;
+			break;
+		case 81: //Q
+		case 113: //q
+			robot.movement.anticlockwise = true;
+			break;
+		case 87: //W
+		case 119: //w
+			robot.movement.clockwise = true;
+			break;*/
 	}
 }
-
-function onResize() {
-	'use strict';
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	scene.activeCamera.aspect = (renderer.getSize().width / renderer.getSize().height);
-	scene.activeCamera.updateProjectionMatrix();
-}
-
 
 function createScene() {
 	'use strict';
@@ -41,22 +58,24 @@ function createScene() {
 	scene.add(new THREE.AxisHelper(10));
 	
 	createRobot(0, 0, 0);
-	createTarget(50, -5, 0);
 	//createTarget(0, 0, 0);
 }
 
 function render() {
 	'use strict';
-	renderer.render(scene, scene.activeCamera);
+	renderer.render(scene, camera);
 }
 
-function animate() {
+function animate(){
 	'use strict';
 
+	robotMovement();
+
 	render();
-	
-	requestAnimationFrame(animate);
+     
+    requestAnimationFrame(animate);
 }
+
 
 function init() {
 	'use strict';
@@ -71,13 +90,11 @@ function init() {
 	createFrontCamera();
 	createTopCamera();
 	createSideCamera();
-	scene.activeCamera = TopCamera;
+	scene.setActiveCamera = 
 
+	
 	render();
-
+	
 	window.addEventListener("resize", onResize);
-	window.addEventListener("keydown", onKeyDown);	
-
-
-
+	window.addEventListener("keydown", onKeyDown);
 }
