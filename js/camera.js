ee4e5e90eaa3eaff57a7cad0beddee6ed617919b
Oscar Stigzelius <oscar.stigzelius@aalto.fi>
2019-09-30 15:13:15 +0100

    moved the camera to be 100px away from 0,0,0 #6

--- js/camera.js
+++ js/camera.js
@@ -2,19 +2,19 @@ var FrontCamera, SideCamera, TopCamera;
 
 function createFrontCamera() {
 	'use strict';
-	
-	FrontCamera = new THREE.OrthographicCamera(-window.innerWidth /  8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight /  8, 1, 1000);
+
+	FrontCamera = new THREE.OrthographicCamera(-window.innerWidth / 8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight / 8, 1, 1000);
 	FrontCamera.position.x = 0;
 	FrontCamera.position.y = 0;
-	FrontCamera.position.z = 50;
+	FrontCamera.position.z = 100;
 	FrontCamera.lookAt(scene.position);
 }
 
 function createSideCamera() {
 	'use strict';
-	
-	SideCamera = new THREE.OrthographicCamera(-window.innerWidth /  8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight /  8, 1, 1000);
-	SideCamera.position.x = 50;
+
+	SideCamera = new THREE.OrthographicCamera(-window.innerWidth / 8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight / 8, 1, 1000);
+	SideCamera.position.x = 100;
 	SideCamera.position.y = 0;
 	SideCamera.position.z = 0;
 	SideCamera.lookAt(scene.position);
@@ -22,10 +22,10 @@ function createSideCamera() {
 
 function createTopCamera() {
 	'use strict';
-	
-	TopCamera = new THREE.OrthographicCamera(-window.innerWidth /  8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight /  8, 1, 1000);
+
+	TopCamera = new THREE.OrthographicCamera(-window.innerWidth / 8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight / 8, 1, 1000);
 	TopCamera.position.x = 0;
-	TopCamera.position.y = 50;
+	TopCamera.position.y = 100;
 	TopCamera.position.z = 0;
 	TopCamera.lookAt(scene.position);
 }
